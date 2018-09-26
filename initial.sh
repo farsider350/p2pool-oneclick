44a28c37a4b087e1a8a664b3bcf896ae5f59318a
farsider350 <31876349+farsider350@users.noreply.github.com>
2018-09-26 16:14:09 +1000

    test

--- initial.sh
+++ initial.sh
@@ -1,5 +1,4 @@
-cd ~
-git clone https://github.com/farsider350/p2pool-oneclick.git
+git clone https://github.com/farsider350/p2pool-oneclick.git ~/
 cd p2pool-oneclick
 chmod a+x install.sh
 ./install.sh
