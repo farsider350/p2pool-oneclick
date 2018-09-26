cb766d441eba65a8ab9c1e53c87dd034a4857e40
farsider350 <31876349+farsider350@users.noreply.github.com>
2018-09-26 15:49:34 +1000

    Testing

--- bitcoin.sh
+++ bitcoin.sh
@@ -1,2 +1,8 @@
 mkdir ~/.bitcoin/
 sudo cp -i bitcoin.conf ~/.bitcoin/
+sudo cp -i btcp2pool.desktop ~/Desktop/
+cd ~
+cd Desktop
+chmod a+x btcp2pool.desktop
+cd ~
+cd p2pool-oneclick
--- litecoin.sh
+++ litecoin.sh
@@ -1,3 +1,8 @@
 mkdir ~/.litecoin/
 sudo cp -i litecoin.conf ~/.litecoin/
 sudo cp -i ltcp2pool.desktop ~/Desktop/
+cd ~
+cd Desktop
+chmod a+x ltcp2pool.desktop
+cd ~
+cd p2pool-oneclick
