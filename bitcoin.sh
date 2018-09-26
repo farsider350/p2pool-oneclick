071a16f68e75b18919cd66c88bc851f883fef374
farsider350 <31876349+farsider350@users.noreply.github.com>
2018-09-26 17:40:22 +1000

    Permissions

--- bitcoin.sh
+++ bitcoin.sh
@@ -1,3 +1,5 @@
 mkdir ~/.bitcoin/
 sudo cp -i bitcoin.conf ~/.bitcoin/
-cd ~/p2pool-oneclick
+cd ~/p2pool-oneclick/Start-P2pool
+chmod a+x ltcp2pool.desktop
+cd ..
--- litecoin.sh
+++ litecoin.sh
@@ -1,3 +1,5 @@
 mkdir ~/.litecoin/
 sudo cp -i litecoin.conf ~/.litecoin/
-cd ~/p2pool-oneclick
+cd ~/p2pool-oneclick/Start-P2pool
+chmod a+x btcp2pool.desktop
+cd ..
