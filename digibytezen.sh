2926cf99b8b77d0ba189ea864c463a4c83b69514
farsider350 <31876349+farsider350@users.noreply.github.com>
2018-09-27 18:40:24 +1000

    Final

---
+++ digibytezen.sh
@@ -0,0 +1,3 @@
+cd ~/p2pool-oneclick/Start-P2pool
+chmod a+x dgbp2poolzen.desktop
+cd ..
--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ chmod a+x dgbp2pool.sh
 chmod a+x digibyte.sh
 ./digibyte.sh
 
+chmod a+x dgbp2poolzen.sh
+chmod a+x digibytezen.sh
+./digibytezen.sh
+
 
 echo "98% Complete"
 echo "99% Complete"
