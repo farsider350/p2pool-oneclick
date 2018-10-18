5277f9e26b913babdd6e728a567224762e41cffa
farsider350 <31876349+farsider350@users.noreply.github.com>
2018-10-18 15:31:22 +1000

    peseta to pesetacoin

--- ltcmerge.sh
+++ ltcmerge.sh
@@ -11,7 +11,7 @@ sudo cp -i viacoin.conf ~/.viacoin/
 sudo cp -i artiqox.conf ~/.artiqox/
 sudo cp -i argentum.conf ~/.argentum/
 sudo cp -i myriadcoin.conf ~/.myriadcoin/
-sudo cp -i peseta.conf ~/.pesetacoin/
+sudo cp -i pesetacoin.conf ~/.pesetacoin/
 cd ~/p2pool-oneclick/Start-P2pool
 chmod a+x ltcp2pool.desktop
 chmod a+x ltcp2poolmerge.desktop
