ebe78d7a26d7903b9ab5ebe82a1328ad65452468
farsider350 <31876349+farsider350@users.noreply.github.com>
2019-05-15 22:34:18 +1000

    Remove 2 chain support

--- dgbp2pool.sh
+++
@@ -1,2 +0,0 @@
-rm -rf ~/p2pool-dgb-scrypt-ils/data/digibyteils
-pypy ~/p2pool-dgb-scrypt-ils/run_p2pool.py --net digibyteils --give-author 0 --merged http://doge:oneclickpass@127.0.0.1:7332 --merged http://via:oneclickpass@127.0.0.1:5222 --merged http://artiqox:oneclickpass@127.0.0.1:19421 --merged http://myriad:oneclickpass@127.0.0.1:10889 --merged http://argentum:oneclickpass@127.0.0.1:13581
